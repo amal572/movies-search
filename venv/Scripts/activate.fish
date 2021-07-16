a975293994f726de16774f1f85ac11c3b46d4542
baraa alsheekha <baraash75@gmail.com>
2021-07-16 18:58:57 +0300

    .

--- venv/Scripts/activate.fish
+++ venv/Scripts/activate.fish
@@ -57,7 +57,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV 'C:\Users\Classic\Desktop\moviefinall\venv'
+set -gx VIRTUAL_ENV 'C:\Users\Baraa\Desktop\moviefinall\venv'
 
 # https://github.com/fish-shell/fish-shell/issues/436 altered PATH handling
 if test (echo $FISH_VERSION | head -c 1) -lt 3
