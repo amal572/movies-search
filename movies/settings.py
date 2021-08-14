88c38b8062d1337f7f8f7ce8005fb7438dd569cd
EC2 Default User <ec2-user@ip-10-10-200-8.eu-west-1.compute.internal>
2021-08-14 21:28:31 +0000

    .

--- movies/settings.py
+++ movies/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'search',
     'origin',
     'categoriesfilm',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -57,7 +58,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'movies.urls'
 
