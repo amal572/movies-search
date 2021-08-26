ced3587050b37eac3a5086dafe99b3daff340d37
baraa65 <baraash65@hotmail.com>
2021-08-26 16:31:33 +0300

    fix bug

--- film/views.py
+++ film/views.py
@@ -138,34 +138,34 @@ class FilmDelete(APIView):
             return Response({'error': 'Something went wrong'}, safe=False, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
-class FilmAddToDatabase(APIView):
-    def post(self, request, format=None):
-        try:
-            data=request.data
-            origins_id=0
-            if origin.objects.filter(name=data['origins']).count()==0:
-                o = origin(name=data['origins'])
-                o.save()
-                data['origins']=o.id
-            else:
-                o = origin.objects.filter(name=data['origins']).values_list('id', flat=True)[0]
-                data['origins']=o           
-            serializer = FilmSerializer(data=data)
-            if serializer.is_valid():
-                serializer.save() 
+# class FilmAddToDatabase(APIView):
+#     def post(self, request, format=None):
+#         try:
+#             data=request.data
+#             origins_id=0
+#             if origin.objects.filter(name=data['origins']).count()==0:
+#                 o = origin(name=data['origins'])
+#                 o.save()
+#                 data['origins']=o.id
+#             else:
+#                 o = origin.objects.filter(name=data['origins']).values_list('id', flat=True)[0]
+#                 data['origins']=o           
+#             serializer = FilmSerializer(data=data)
+#             if serializer.is_valid():
+#                 serializer.save() 
 
-            return Response(serializer)
+#             return Response(serializer)
             
-        except ObjectDoesNotExist as e:
-            return Response({'error': str(e)}, safe=False, status=status.HTTP_404_NOT_FOUND)
-        except Exception:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-            
+#         except ObjectDoesNotExist as e:
+#             return Response({'error': str(e)}, safe=False, status=status.HTTP_404_NOT_FOUND)
+#         except Exception:
+#             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 class FilmAddToDatabase(APIView):
     def post(self, request, format=None):
         try:
             data=request.data
-            return Response(data)
+            
             origins_id=0
             if origin.objects.filter(name=data['origins']).count()==0:
                 o = origin(name=data['origins'])
@@ -173,7 +173,8 @@ class FilmAddToDatabase(APIView):
                 data['origins']=o.id
             else:
                 o = origin.objects.filter(name=data['origins']).values_list('id', flat=True)[0]
-                data['origins']=o           
+                data['origins']=o   
+            return Response(data)
             serializer = FilmSerializer(data=data)
             if serializer.is_valid():
                 serializer.save() 
