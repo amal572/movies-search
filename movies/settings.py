d1137f9616bbe28594a9fabfcf4bbfe49ab88832
baraa65 <baraash65@hotmail.com>
2021-09-01 22:57:47 +0300

    c

--- movies/settings.py
+++ movies/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -129,6 +130,11 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=10),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=20),
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
