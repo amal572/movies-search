86c4776aafec2ee84bb0627122395016764214cf
baraa65 <baraash65@hotmail.com>
2021-09-01 16:23:44 +0300

    c

--- dsearch/views.py
+++ dsearch/views.py
@@ -94,35 +94,19 @@ def finallsearch(query):
 
 
 class searchApi(APIView):
-    def get(self, request):
-        query = "فيلم أكشن قائم على الذكاء الاصطناعي"
-        # movies = pd.read_csv('example_file.csv')
-        # print(movies)
-        # ranked_results = finallsearch('فيلم أكشن قائم على الذكاء الاصطناعي')
+    def post(self, request):
+        query = request.data['search']
+
         resfinal = translatorsearch1(query)
         model = SentenceTransformer('msmarco-distilbert-base-dot-prod-v3')
         index = faiss.deserialize_index(np.load("test.npy"))
-        print(index)
-        t = time.time()
-        print(1)
+
         query_vector = model.encode([resfinal])
-        print(2)
         top_k = index.search(query_vector, 10)
-        print(top_k)
-        print(3)
         top_k_ids = top_k[1].tolist()[0]
-        print(4)
         top_k_ids = list(np.unique(top_k_ids))
-        print(5)
-        print(top_k_ids)
         results = [fetch_movie_info(idx) for idx in top_k_ids]
-        print(6)
-        # results = search(resfinal, top_k=15, index=index, model=model)
-        print(6.1)
-        # results = search(resfinal, top_k=5, index=index, model=model)
-        # ranked_results_bert = sorted(ranked_results, key=lambda x: x['Score'], reverse=True)
-        print(type(results))
-        Maxfilm = list(results)
+
         listresult = []
         finalres =[]
         for x in results:
@@ -133,12 +117,6 @@ class searchApi(APIView):
             serializers = FilmSerializer(movies, many=True)
             finalres.append(serializers.data[0])
 
-        print(type(finalres))
-
-        # print(7)
-        # print(type(Maxfilm))
-        # print(Maxfilm)
         newMax = json.dumps(finalres)
-        print(8)
-        # print(newMax)
+
         return Response(json.loads(newMax))
